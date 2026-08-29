174ca88ad4b7e9bfd8251323829c180d8946f7fe
human <doe06677@gmail.com>
2026-08-29 16:42:39 -0500

    Initial Commit

---
+++ paperGIF/paperGIFApp.swift
@@ -0,0 +1,17 @@
+//
+//  paperGIFApp.swift
+//  paperGIF
+//
+//  Created by Levi Richards on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct paperGIFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ paperGIFTests/paperGIFTests.swift
@@ -0,0 +1,17 @@
+//
+//  paperGIFTests.swift
+//  paperGIFTests
+//
+//  Created by Levi Richards on 8/29/26.
+//
+
+import Testing
+@testable import paperGIF
+
+struct paperGIFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
